324210ed6286e41ecec7c933c8ecd43cc56b712d
Your Name <you@example.com>
2024-05-08 12:26:07 +0900

    0508

--- boot240417/src/test/java/com/yedam/app/Boot240417ApplicationTests.java
+++ boot240417/src/test/java/com/yedam/app/Boot240417ApplicationTests.java
@@ -1,6 +1,7 @@
 package com.yedam.app;
 
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -34,4 +35,20 @@ class Boot240417ApplicationTests {
 		
 	}
 	
+	@Autowired
+	StringEncryptor jasyptStringEncryptor;
+	
+	@Test
+	public void encryption() {
+		String[] strs = {
+				"net.sf.log4jdbc.sql.jdbcapi.DriverSpy",
+				"jdbc:log4jdbc:oracle:thin:@127.0.0.1:1521:xe",
+				"hr",
+				"hr"
+		};
+		for(String str : strs) {
+			String encStr = jasyptStringEncryptor.encrypt(str);
+			System.out.println(encStr);
+		}
+	}
 }
